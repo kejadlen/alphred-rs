fe33cac89fb63076be383cfc8d98d053021d1aaf
Alpha Chen <alpha.chen@gmail.com>
2017-10-12 08:57:01 -0700

    Initial commit
    Just the serializable structs.

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#[macro_use]
+extern crate serde_derive;
+
+use std::path::PathBuf;
+
+#[derive(Debug, Serialize)]
+pub struct Item {
+    pub title: String,
+    pub subtitle: String,
+    pub arg: String,
+    pub icon: Icon,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Icon {
+    pub path: PathBuf,
+}
