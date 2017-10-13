f311a2f453d40ed4e1ca4dd833d2d8131c1d232a
Alpha Chen <alpha.chen@gmail.com>
2017-10-12 21:33:06 -0700

    Use a builder pattern for Item

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,30 @@ use std::path::PathBuf;
 
 #[derive(Debug, Serialize)]
 pub struct Item {
-    pub title: String,
-    pub subtitle: String,
-    pub arg: String,
-    pub icon: Icon,
+    title: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    subtitle: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    arg: Option<String>,
+    icon: Icon,
+}
+
+impl Item {
+    fn new(title: String, icon: Icon) -> Self {
+        let subtitle = None;
+        let arg = None;
+        Item { title, subtitle, arg, icon }
+    }
+
+    fn subtitle<'a>(&'a mut self, subtitle: &str) -> &'a mut Self {
+        self.subtitle = Some(subtitle.into());
+        self
+    }
+
+    fn arg<'a>(&'a mut self, arg: &str) -> &'a mut Self {
+        self.arg = Some(arg.into());
+        self
+    }
 }
 
 #[derive(Debug, Serialize)]
